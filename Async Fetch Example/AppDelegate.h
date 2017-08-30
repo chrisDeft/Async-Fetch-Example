8f5e0fb079d2964fe74a8ddc279a32ca23439794
Chris Adamson <deft@chriss-macbook-air.local>
2017-08-30 11:35:05 +0100

    Initial Commit

---
+++ Async Fetch Example/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Async Fetch Example
+//
+//  Created by Chris Adamson on 30/08/2017.
+//  Copyright © 2017 Chris Adamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Async Fetch Example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Async Fetch Example
+//
+//  Created by Chris Adamson on 30/08/2017.
+//  Copyright © 2017 Chris Adamson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
